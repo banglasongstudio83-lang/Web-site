9870f26598dd1d43cae0ddd979127f5365884873
banglasongstudio83-lang <banglasongstudio83@gmail.com>
2026-08-05 20:08:48 +0600

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,31 @@
+export default function Home() {
+  return (
+    <main className="container">
+
+      <h1
+      style={{
+      textAlign:"center",
+      marginTop:"40px",
+      color:"#0F766E"
+      }}
+      >
+
+      👶 Baby Family Blog
+
+      </h1>
+
+      <p
+      style={{
+      textAlign:"center",
+      marginTop:"20px",
+      fontSize:"18px"
+      }}
+      >
+
+      বাংলা শিশু ও পরিবার বিষয়ক ব্লগে আপনাকে স্বাগতম।
+
+      </p>
+
+    </main>
+  );
+}
