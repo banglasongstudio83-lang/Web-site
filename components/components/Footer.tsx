2d0f8b44f24abbba5a580969c6261bafee559ff5
banglasongstudio83-lang <banglasongstudio83@gmail.com>
2026-08-05 20:11:21 +0600

    Create Footer.tsx

---
+++ components/components/Footer.tsx
@@ -0,0 +1,18 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+
+      <div className="container">
+
+        <h3>Baby Family Blog</h3>
+
+        <p>
+          © 2026 Baby Family Blog.
+          All Rights Reserved.
+        </p>
+
+      </div>
+
+    </footer>
+  );
+}
