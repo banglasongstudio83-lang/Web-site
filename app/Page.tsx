fcb1f5f0aee7b0979fc72efd960016200a78b072
banglasongstudio83-lang <banglasongstudio83@gmail.com>
2026-08-05 21:01:49 +0600

    Update Page.tsx

--- app/Page.tsx
+++ app/Page.tsx
@@ -2,16 +2,14 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import SearchBox from "@/components/SearchBox";
 import FeaturedPosts from "@/components/FeaturedPosts";
-import CategoryCards from "@/components/CategoryCards";
-import LatestArticles from "@/components/LatestArticles";
 import Sidebar from "@/components/Sidebar";
+
 export default function Home() {
   return (
     <>
       <Header />
 
       <main className="container">
-
         <section className="hero">
           <h1>👶 Baby Family Blog</h1>
 
@@ -23,9 +21,13 @@ export default function Home() {
 
         <SearchBox />
 
-        <FeaturedPosts />
-        <CategoryCards />
-        <LatestArticles />
+        <div className="content-layout">
+          <div className="main-content">
+            <FeaturedPosts />
+          </div>
+
+          <Sidebar />
+        </div>
       </main>
 
       <Footer />
