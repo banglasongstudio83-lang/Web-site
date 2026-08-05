e2bf1c68ac0998f3db69321acab214462e50dfd6
banglasongstudio83-lang <banglasongstudio83@gmail.com>
2026-08-05 20:21:29 +0600

    Create SearchBox.tsx

---
+++ components/SearchBox.tsx
@@ -0,0 +1,12 @@
+export default function SearchBox() {
+  return (
+    <div className="search-box">
+      <input
+        type="text"
+        placeholder="🔍 ব্লগ খুঁজুন..."
+      />
+
+      <button>Search</button>
+    </div>
+  );
+}
